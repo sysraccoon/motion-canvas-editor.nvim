b8226a93e1d7aa1890e40970c4968b549a8a667e
sysraccoon <sysraccoon@gmail.com>
2025-01-18 19:55:52 +0300

    add basic plugin implementation

---
+++ lua/motion-canvas-editor/internals/core.lua
@@ -0,0 +1,103 @@
+local Core = {}
+
+local nvim_set_visual_mode_by_key = function(key)
+	if key ~= "v" and key ~= "V" then
+		return error("invalid visual mode key")
+	end
+
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), "x", true)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, false, true), "x!", true)
+end
+
+local function clamp(value, min, max)
+	return math.min(math.max(value, min), max)
+end
+
+Core.get_active_selection = function(bufnr)
+	local mode = vim.api.nvim_get_mode().mode
+	if mode == "v" or mode == "V" then
+		Core.nvim_set_normal_mode()
+		local sel_start_row, sel_start_col = unpack(vim.api.nvim_buf_get_mark(bufnr, "<"))
+		local sel_end_row, sel_end_col = unpack(vim.api.nvim_buf_get_mark(bufnr, ">"))
+		nvim_set_visual_mode_by_key(mode)
+
+		return {
+			start_row = sel_start_row - 1, -- fix 1-based index
+			start_col = sel_start_col,
+			end_row = sel_end_row - 1, -- fix 1-based index
+			end_col = sel_end_col,
+		}
+	end
+
+	return nil
+end
+
+Core.get_relative_buffer_name = function(bufnr)
+	if type(bufnr) ~= "number" or bufnr % 1 ~= 0 then
+		return error(vim.inspect(bufnr) .. ": invalid bufnr type")
+	end
+
+	return vim.fn.expand("#" .. bufnr .. ":.")
+end
+
+Core.get_buffer_content = function(bufnr)
+	local buffer_line_count = vim.api.nvim_buf_line_count(bufnr)
+	local buffer_lines = vim.api.nvim_buf_get_lines(bufnr, 0, buffer_line_count, false)
+	local buffer_content = table.concat(buffer_lines, "\n")
+	return buffer_content
+end
+
+Core.nvim_set_normal_mode = function()
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), "x", true)
+end
+
+Core.nvim_set_visual_mode = function()
+	nvim_set_visual_mode_by_key("v")
+end
+
+Core.nvim_set_visual_line_mode = function()
+	nvim_set_visual_mode_by_key("V")
+end
+
+Core.clamp_row_range = function(bufnr, row, size)
+	if size <= 0 then
+		return error(vim.inspect(size) .. " size should be greater than 0")
+	end
+
+	if row < 0 then
+		return error(vim.inspect(row) .. " start_row should be greater or equal 0")
+	end
+
+	local line_count = vim.api.nvim_buf_line_count(bufnr)
+	local start_row = clamp(row, 0, line_count - 1)
+	local end_row = clamp(start_row + size - 1, 0, line_count - 1)
+
+	return {
+		start_row = start_row,
+		end_row = end_row,
+	}
+end
+
+Core.set_viewport_region = function(bufnr, start_row, size, hl_group)
+	local viewport_region = Core.clamp_row_range(bufnr, start_row, size)
+
+	if not viewport_region then
+		return error("invalid viewport region")
+	end
+
+	local hl_start = vim.api.nvim_buf_set_extmark(bufnr, ns, viewport_region.start_row, 0, {
+		line_hl_group = hl_group,
+	})
+	local hl_end = vim.api.nvim_buf_set_extmark(bufnr, ns, viewport_region.end_row, 0, {
+		line_hl_group = hl_group,
+	})
+
+	return {
+		hl_start = hl_start,
+		hl_end = hl_end,
+		row_start = viewport_region.start_row,
+		row_end = viewport_region.end_row,
+	}
+end
+
+return Core
---
+++ tests/motion-canvas-editor_spec.lua
@@ -0,0 +1,112 @@
+local assert = require("luassert")
+
+local create_buffer = function(name, content)
+	local bufnr = vim.api.nvim_create_buf(true, true)
+	assert(bufnr ~= 0, "failed to create temp buffer")
+
+	vim.api.nvim_buf_set_name(bufnr, name)
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, content)
+	return bufnr
+end
+
+local split_lines = function(str)
+	local result = {}
+	for line in str:gmatch("[^\n]+") do
+		table.insert(result, line)
+	end
+	return result
+end
+
+describe("motion-canvas-editor", function()
+	it("can be required", function()
+		require("motion-canvas-editor")
+	end)
+end)
+
+describe("motion-canvas-editor.internals.core", function()
+	local core = require("motion-canvas-editor.internals.core")
+
+	local buf_name = "dummy.lua"
+	local buf_content = split_lines([[
+    function hello()
+      print('hello lua')
+    end
+  ]])
+	local bufnr = create_buffer(buf_name, buf_content)
+
+	it("change nvim mode to normal", function()
+		core.nvim_set_normal_mode()
+		assert.are.equal("n", vim.api.nvim_get_mode().mode)
+	end)
+
+	it("change nvim mode to visual", function()
+		core.nvim_set_visual_mode()
+		assert.are.equal("v", vim.api.nvim_get_mode().mode)
+	end)
+
+	it("change nvim mode to visual line", function()
+		core.nvim_set_visual_line_mode()
+		assert.are.equal("V", vim.api.nvim_get_mode().mode)
+	end)
+
+	it("return active selection in visual mode", function()
+		local expected_selection = {
+			start_row = 1,
+			start_col = 9,
+			end_row = 1,
+			end_col = 17,
+		}
+
+		core.nvim_set_visual_mode()
+
+		vim.api.nvim_buf_set_mark(bufnr, "<", expected_selection.start_row + 1, expected_selection.start_col, {})
+		vim.api.nvim_buf_set_mark(bufnr, ">", expected_selection.end_row + 1, expected_selection.end_col, {})
+
+		local selection = core.get_active_selection(bufnr)
+
+		local current_mode = vim.api.nvim_get_mode().mode
+
+		assert.are.equal("v", current_mode)
+		assert.are.same(expected_selection, selection)
+
+		core.nvim_set_normal_mode()
+	end)
+
+	it("return empty selection in normal mode", function()
+		core.nvim_set_normal_mode()
+
+		local selection = core.get_active_selection(bufnr)
+		assert.is_nil(selection)
+	end)
+
+	it("return buffer content as string", function()
+		local expected_content = table.concat(buf_content, "\n")
+		local actual_content = core.get_buffer_content(bufnr)
+
+		assert.are.equal(expected_content, actual_content)
+	end)
+
+	it("return relative buffer name", function()
+		local actual_name = core.get_relative_buffer_name(bufnr)
+		assert.are.equal(buf_name, actual_name)
+	end)
+
+	it("should throw error when try get relative buffer name for non integer argument", function()
+		assert.are.error(function()
+			core.get_relative_buffer_name(0.5)
+		end)
+
+		assert.are.error(function()
+			core.get_relative_buffer_name("test")
+		end)
+	end)
+
+	it("clamp row range", function()
+		local expected_range = {
+			start_row = 1,
+			end_row = 3,
+		}
+		local actual_range = core.clamp_row_range(bufnr, 1, 20)
+		assert.are.same(expected_range, actual_range)
+	end)
+end)
